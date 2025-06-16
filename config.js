2930a0dbc4947a49304dd70d7e62da962a43f8a4
XxXcatXxX <260206363@qq.com>
2025-06-16 12:28:28 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCbwFWC_CgwBpWrB35dJb0wLsPWTCho85U",
+    authDomain: "runaway-ai-28d72.firebaseapp.com",
+    projectId: "runaway-ai-28d72",
+    storageBucket: "runaway-ai-28d72.firebasestorage.app",
+    messagingSenderId: "274100245683",
+    appId: "1:274100245683:web:07078268e1f181d0b58cc6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
